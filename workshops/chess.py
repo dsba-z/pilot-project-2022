cbdfbcaeb982e85a4eb090cfe73b356426a8500a
Georgii Zhulikov <l8doku@gmail.com>
2022-09-16 11:13:25 +0300

    Split condition into variables

--- workshops/chess.py
+++ workshops/chess.py
@@ -1,5 +1,7 @@
 def knight_single_move(input_string):
     x1, y1, x2, y2 = [int(x) for x in input_string.split()]
-    if (abs(x1 - x2) == 1 and abs(y1 - y2) == 2) or (abs(x1 - x2) == 2 and abs(y1 - y2) == 1):
+    vertical = abs(x1 - x2) == 1 and abs(y1 - y2) == 2
+    horizontal = abs(x1 - x2) == 2 and abs(y1 - y2) == 1
+    if vertical or horizontal:
         return True
     return False

eb9d9903631fe23222249a5594c0cb923447fd1a
spnya <alexander-ukhanov@yandex.ru>
2022-09-21 14:11:06 +0300

    numerical systems fix, knight problem, intrest rate problem

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from workshops.numeral_systems import convert
 from workshops.conditions_and_loops import solve_the_knight_move_problem
+from workshops.real_numbers_and_strings import interest_rate_wrapper
 
 greeting = '''Choose option:
 1. Convert number from any base to any base.
 2. Solve the knight move problem
-3. Exit
+3. Solve interest rate problem
+4. Exit
 '''
 
 
@@ -21,4 +23,6 @@ while True:
     elif user_input == '2':
         pass_input(solve_the_knight_move_problem)
     elif user_input == '3':
+        pass_input(interest_rate_wrapper)
+    elif user_input == '4':
         break
--- workshops/conditions_and_loops.py
+++ workshops/conditions_and_loops.py
@@ -1,8 +1,13 @@
-def solve_the_knight_move_problem(input_string):
+def can_knight_move(input_string):
     x1, y1, x2, y2 = map(int, input_string.split())
-    if ((abs(x1-x2) == 1 and abs(y1-y2) == 2) or
-            (abs(x1-x2) == 2 and abs(y1-y2) == 1)):
+    if ((abs(x1 - x2) == 1 and abs(y1 - y2) == 2) or
+            (abs(x1 - x2) == 2 and abs(y1 - y2) == 1)):
+        return True
+    else:
+        return False
+
+def solve_the_knight_move_problem(input_string):
+    if can_knight_move(input_string):
         return "Yes, it can"
     else:
         return "No, it cannot"
-
